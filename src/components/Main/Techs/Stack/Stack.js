7c867cebc91a87d24ba07397fb0dfcce5d71efcc
Tatyana Izmailova <rutaizm@yandex.ru>
2022-09-13 12:11:41 +0300

    techs

---
+++ src/components/Main/Techs/Stack/Stack.js
@@ -0,0 +1,17 @@
+import './Stack.css'
+
+function Stack() {
+    return(
+        <ul className='stack'>
+            <li className='stack__item'>HTML</li>
+            <li className='stack__item'>CSS</li>
+            <li className='stack__item'>JS</li>
+            <li className='stack__item'>React</li>
+            <li className='stack__item'>Git</li>
+            <li className='stack__item'>Express.js</li>
+            <li className='stack__item'>mongoDB</li>
+        </ul>
+   )
+}
+
+export default Stack;
