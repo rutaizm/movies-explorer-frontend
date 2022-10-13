c08bbb4e901e3af9e121fdedf4083977b703d5fd
Tatyana Izmailova <rutaizm@yandex.ru>
2022-10-13 12:53:42 +0300

    added screensize dependency for show more button

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,20 +2,51 @@ import React from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList.js';
 import Preloader from '../Preloader/Preloader.js';
+import { useScreenSize } from '../../hooks/useScreenSize';
 
 function Movies({foundedCards,isNoMoviesMessage,onSearch, onLike, onDelete, isChecked, renderLoading, onClick, savedMovies, request}) {
 
+    const screenSize = useScreenSize();
+
     const [ moviesCardsList, setMoviesCardsList] = React.useState([]);
     const [showMore, setShowMore] = React.useState(false);
+    const [showOnClick, setShowOnClick] = React.useState(null);
+    const [showOnStart, setShowOnStart]= React.useState(null);
     
+React.useEffect(() => {
+        if (screenSize > 1280) {
+            setShowOnClick(3);
+            setShowOnStart(3);
+        } else if (screenSize > 1024) {
+            setShowOnClick(3);
+            setShowOnStart(3);
+        } else if (screenSize > 768) {
+            setShowOnClick(2);
+            setShowOnStart(8);
+        } else if (screenSize > 320) {
+            setShowOnClick(2);
+            setShowOnStart(5);
+        }
+      }, [screenSize]);
+
     function handleShowMorePosts() {        
-        setMoviesCardsList(foundedCards.slice(0, moviesCardsList.length + 2))
+        setMoviesCardsList(foundedCards.slice(0, moviesCardsList.length + showOnClick))
+        if (moviesCardsList.length >= moviesCardsList.length - showOnClick) {
+            setShowMore(false);
+        }
     }
 
     React.useEffect(() =>{
-        setMoviesCardsList(foundedCards.slice(0, 2)
-        );
-    }, [foundedCards])
+        if (foundedCards.length <= showOnStart) {
+            setShowMore(false);                        
+        } if (foundedCards.length > showOnStart) {
+            setMoviesCardsList(foundedCards.slice(0, showOnStart));
+            setShowMore(true);
+        } else {
+            setMoviesCardsList(foundedCards);
+            setShowMore(false);
+        }         
+    }, [foundedCards, showOnStart])
 
     return(
         <>
@@ -44,3 +75,4 @@ function Movies({foundedCards,isNoMoviesMessage,onSearch, onLike, onDelete, isCh
 
 export default Movies;
 
+  
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
-import { useScreenSize } from '../../hooks/useScreenSize';
+
 
 function MoviesCardList({showMore, handleShowMorePosts, cardsToRender, isNoMoviesMessage, onLike, onDelete, onClick, savedMovies}) {   
     
-    const screenSize = useScreenSize();
+   
     const infoMessage = isNoMoviesMessage ? 'moviescard__message moviescard__message_type_active' : 'moviescard__message'
  
     const showMoreClassname = showMore? 'moviescard__more-button moviescard__more-button_type_active' : 'moviescard__more-button';
-    // React.useEffect(() => {
-    //     if (screenSize > 1280) {
-    //       console.log('big')
-    //     } else if (screenSize > 1024) {
-    //         console.log('middle')
-    //     } else if (screenSize > 768) {
-    //         console.log('small')
-    //     } else if (screenSize > 320) {
-    //         console.log('xtrasmall')
-    //     }
-    //   }, [screenSize]);
-      
+       
 
     return(
         <section className='moviescard'>    
