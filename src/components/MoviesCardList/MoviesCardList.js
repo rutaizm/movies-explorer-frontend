3935531025829b9b940f7640ae837921fd8fbbb8
Tatyana Izmailova <rutaizm@yandex.ru>
2022-10-16 11:56:32 +0300

    movies list correction

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
-import {useLocation} from  'react-router';
 
 
-function MoviesCardList({showMore, foundedSavedCards, handleShowMorePosts, cardsToRender, isNoMoviesMessage, onLike, onDelete, savedMovies}) {   
-    
-    const location = useLocation();
-   
+function MoviesCardList({showMore, handleShowMorePosts, cardsToRender, isNoMoviesMessage, onLike, onDelete, savedMovies}) {   
+       
     const infoMessage = isNoMoviesMessage ? 'moviescard__message moviescard__message_type_active' : 'moviescard__message'
  
     const showMoreClassname = showMore? 'moviescard__more-button moviescard__more-button_type_active' : 'moviescard__more-button';
@@ -19,32 +16,15 @@ function MoviesCardList({showMore, foundedSavedCards, handleShowMorePosts, cards
                 <p className={infoMessage}>{isNoMoviesMessage}</p>
             }       
             <ul className='moviescard__list'>
-
-                {location.pathname === '/movies' &&
-                    (cardsToRender.map((film) =>
-                    (<MoviesCard
-                            key={film.id || film._id}
-                            movie={film}
-                            onDelete={onDelete}
-                            onLike={onLike}
-                            savedMovies={savedMovies}
-                        />)  
-                    ))
-                }
-                {location.pathname === '/saved-movies' && 
-                    (cardsToRender.map((film) =>
-                    (<MoviesCard
-                            key={film.id || film._id}
-                            movie={film}
-                            onDelete={onDelete}
-                            onLike={onLike}
-                            savedMovies={savedMovies}
-                        />)
-                    ))
-                }
-
-
-
+                {cardsToRender.map((film) =>
+                        (<MoviesCard
+                                key={film.id || film._id}
+                                movie={film}
+                                onDelete={onDelete}
+                                onLike={onLike}
+                                savedMovies={savedMovies}
+                            />)
+                        )}
             </ul>
             <button 
                 type='button' 
